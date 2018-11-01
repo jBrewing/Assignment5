743ca33fa985ba0ed294914982296ced979a3d41
JBrewer <jbrewer256@gmail.com>
2018-11-01 13:07:23 -0600

    Got pseudo code into query section.  Need to visualize how the query will build the lists for simultaneous presentation.

---
+++ source.py
@@ -0,0 +1,54 @@
+#
+#_____________________________________________________
+#
+# Assignment 5 - Using Python to Retreive and Visualize Data
+# Created by: Joseph Brewer
+# FOR: CEE6110 - Hydroinformatics - Horsburgh
+# DUE: 11/6/2018
+# Description: This script will present a month by month
+# water temperature summary from the Logan River ODM database,
+# complete with a 4 pane plot describing and
+# illustrating a variety of statistics for the selected site and month.
+#____________________________________________
+
+
+#PseudoCode
+
+# 1. Import packages
+#       - connecting to sql server -> sqlite3
+#       - making plots             -> matplotlib.pyplot
+#       - performing stats         -> pandas?
+
+# 2. Receive input
+#       - variables:
+#              1. site ID
+#              2. monthID
+#              3. yearID
+#              4.
+#
+
+# 3. Query database
+#    a. build query
+#       - filter by month
+#   - filter by year
+#- pull all data values
+#
+#
+#
+
+#    b. establish connection
+#    c. execute query
+
+# 4. Transform data into usable lists
+
+# 5. Perform calculations
+#    a. time series plot for month
+#    b. histogram with curve
+#    c. stats.
+#    d. year to year comparison
+
+# 6. Import into graph
+
+# 7. Output graph
+#    a. seperate window
+#    b. save graph
